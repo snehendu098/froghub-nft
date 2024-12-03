e6a6d67036e187df071e46a0027025a25a45aa6f
snehendu098 <roysnehendupersonal@gmail.com>
2024-12-03 20:12:27 +0530

    dynamic page to search

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -2,6 +2,8 @@ import { SearchResults } from "@/components/core";
 import { fetchEvents } from "@/lib/db-operation";
 import React, { Suspense } from "react";
 
+export const dynamic = "force-dynamic";
+
 const Page = async () => {
   const allEvents = await fetchEvents();
 
